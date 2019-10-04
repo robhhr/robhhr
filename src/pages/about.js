6b9ba12e59cbd998b552f351ab4460172f4bf4b4
Roberto H <robhhr@outlook.com>
2019-10-04 04:48:41 -0700

    create initial pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 export default () => (
   <div>
-    <h1>Hello world!</h1>
-    <Link to="/about/">About</Link>
+    <h1>About page</h1>
+    <Link to="/">Home</Link>
   </div>
 )

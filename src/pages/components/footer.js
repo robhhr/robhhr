e3cc3399b07c4690cf7b9de4ef54e1b8179ee33c
Roberto H <robhhr@outlook.com>
2019-10-14 03:09:47 -0700

    style footer responsive divider

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -12,8 +12,8 @@ const Footer = () => {
         position: fixed;
         bottom: 0;
         padding: 10px 0;
-        border-top: 0.5px solid ${colors.border_gray};
-      `}
+        /* border-top: 0.5px solid ${colors.border_gray}; */
+        box-shadow: 0 -2px 5px rgba(0,0,0,.04);      `}
     >
       <ul
         css={css`

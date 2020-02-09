2821518dffa910fcc772b8620db0ab89af69e52f
Roberto H <robhhr@outlook.com>
2020-02-09 05:39:26 -0800

    change site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Roberto Hernandez`,
+    description: `Personal portfolio site built with React, Gatsby and GraphQL`,
+    author: `@robhhr`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const App = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Roberto Hernandez | Web Developer" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -18,4 +17,4 @@ const IndexPage = () => (
   </Layout>
 )
 
-export default IndexPage
+export default App

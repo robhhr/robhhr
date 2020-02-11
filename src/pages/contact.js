0c2c998c2a6324e0892c6de0ae77b3dd8e467c11
Roberto H <robhhr@outlook.com>
2020-02-10 19:03:54 -0800

    create contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Layout, NavBar } from "../components"
+
+const Contact = () => {
+  return (
+    <>
+      <NavBar />
+      <Layout>
+        <h1>Contact</h1>
+      </Layout>
+    </>
+  )
+}
+
+export default Contact

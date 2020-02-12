0793dd77cc5d8f33c30fe05c65655d0e77bdda5a
Roberto H <robhhr@outlook.com>
2020-02-11 19:36:29 -0800

    create section title component

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -2,14 +2,31 @@ import React from "react"
 import styled from "styled-components"
 import "../assets/fonts.css"
 
+const StyledSection = styled.h1`
+  font-family: "Cairo", sans-serif;
+  font-size: 1.35rem;
+  font-weight: 500;
+  padding-bottom: 20px;
+
+  &::after {
+    content: " ";
+    border-bottom: 4px solid #464f59;
+    display: block;
+    width: 50px;
+    margin: 10px 0 5px;
+  }
+`
 const StyledSummary = styled.h1`
   font-family: "Poppins", sans-serif;
   font-size: 1.5rem;
   font-weight: 300;
-  line-height: 2;
+  padding: 20px 0;
+  line-height: 1.8;
   width: 80%;
 `
 
+const Section = ({ children }) => <StyledSection>{children}</StyledSection>
+
 const Summary = ({ children }) => <StyledSummary>{children}</StyledSummary>
 
-export { Summary }
+export { Section, Summary }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from "react"
 import { Footer, Layout, NavBar, SEO } from "../components"
-import { Summary } from "../components/Typography"
+import { Section, Summary } from "../components/Typography"
 
 const App = () => (
   <>
     <NavBar />
     <Layout>
       <SEO title="Roberto Hernandez | Web Developer" />
-      <Summary>Hello! I'm Roberto, a web developer based in Vancouver</Summary>
+      <Summary>
+        Hello! I'm Roberto, a{" "}
+        <span style={{ color: "#fc3565" }}>web developer</span> based in
+        Vancouver
+      </Summary>
+      <Section>Featured Projects</Section>
     </Layout>
     <Footer />
   </>

36346efd5a15744aab5f4f69e27043b6dbf07787
robhhr <robhhr@outlook.com>
2020-07-04 20:41:54 -0700

    remove old dependencies

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,56 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
-import Logo from "../images/blueLogo.svg"
-import styled from "styled-components"
-
-const StyledNav = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.04);
-  height: 70px;
-
-  @media (min-width: 850px) {
-    padding: 0px 135px;
-    height: 80px;
-  }
-`
-
-const StyledLogo = styled.img`
-  margin: 0 20px;
-  width: 100px;
-
-  @media (min-width: 850px) {
-    width: 120px;
-  }
-`
-
-const StyledLink = styled(props => <Link {...props} />)`
-  text-decoration: none;
-  margin: 0 14px;
-  font-family: "Poppins", sans-serif;
-  font-size: 14px;
-
-  &:last-child {
-    margin-right: 20px;
-  }
-
-  @media (min-width: 850px) {
-    font-size: 17px;
-    margin-right: 35px;
-  }
-`
 
 const NavBar = () => {
   return (
-    <StyledNav>
-      <Link to="/">
-        <StyledLogo src={Logo} alt="site-logo" />
-      </Link>
-      <div>
-        <StyledLink to="/about">about</StyledLink>
-      </div>
-    </StyledNav>
+    <div>
+      <a>navbar</a>
+    </div>
   )
 }
 
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,39 +1,9 @@
 import React from "react"
-import { createGlobalStyle } from "styled-components"
 import "../assets/fonts.css"
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    position: relative;
-  }
-
-  h1, h2, h3, h4, h5, h6, p, a {
-    font-family: "Poppins", sans-serif;
-    font-size: 1.2rem;
-    color: #222222;
-  }
-
-  main {
-    margin: 0 15px;
-    z-index: 999;
-
-    @media (min-width: 850px) {
-      margin: 40px 150px 60px 150px;
-    }
-  }
-
-  canvas {
-    z-index: -999;
-    height: 100%
-  }
-`
 
 const Wrapper = ({ children }) => {
   return (
     <>
-      <GlobalStyle />
       <main>{children}</main>
     </>
   )

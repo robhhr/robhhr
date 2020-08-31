78cb8261d6c54cc5a54df1a718edd533c594fb61
robhhr <robhhr@outlook.com>
2020-08-30 21:35:46 -0700

    add contact section

---
+++ src/components/Contact.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Git, Mail, Twitter} from '../assets'
+
+const ContactSection = styled.div`
+  margin: 0 5px;
+  padding-bottom: 15px;
+`
+const LinkSection = styled.div`
+  display: flex;
+`
+const StyledLink = styled.a`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+`
+const Title = styled.h1`
+  font-size: 1.5rem;
+  padding-top: 10px;
+`
+
+const Contact = () => {
+  return (
+    <ContactSection>
+      <Title>Contact</Title>
+      <LinkSection>
+        <StyledLink href="https://robhhr.com" target="_blank" rel="noreferrer">
+          <Git /> GitHub
+        </StyledLink>
+        <StyledLink href="mailto:robhhr@outlook.com">
+          <Mail /> Email
+        </StyledLink>
+        <StyledLink
+          href="https://twitter.com/hallorob"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Twitter /> Twitter
+        </StyledLink>
+      </LinkSection>
+    </ContactSection>
+  )
+}
+
+export default Contact
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
+import Contact from './Contact'
 import NavBar from './NavBar'
 import Project from './Project'
 import ProjectSection from './ProjectSection'
 import SEO from './seo'
 import Wrapper from './Wrapper'
 
-export {NavBar, Project, ProjectSection, SEO, Wrapper}
+export {Contact, NavBar, Project, ProjectSection, SEO, Wrapper}

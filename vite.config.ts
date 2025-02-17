105273ae75e31a9ffe1d5daa56e3c2288a247424
robhhr <robertohhr@pm.me>
2025-02-17 07:06:34 -0800

    upgrade project template

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -9,8 +10,7 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
-  plugins: [
-    remix({
+  plugins: [remix({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
@@ -18,7 +18,5 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
-    }),
-    tsconfigPaths(),
-  ],
+  }), netlifyPlugin(), tsconfigPaths()],
 });

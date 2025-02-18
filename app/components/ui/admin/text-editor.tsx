47bd7903b954f529c8fea36ffb9ad2756b0449d5
robhhr <robertohhr@pm.me>
2025-02-18 02:09:30 -0800

    create init text editor - md & vim

--- app/components/ui/admin/text-editor.tsx
+++ app/components/ui/admin/text-editor.tsx
@@ -1,16 +1,147 @@
-import {cx} from 'class-variance-authority'
+import {lazy, Suspense, useCallback, useEffect, useState} from 'react'
+import {markdown, markdownLanguage} from '@codemirror/lang-markdown'
+import {createTheme} from '@uiw/codemirror-themes'
+import {languages} from '@codemirror/language-data'
+import {vim} from '@replit/codemirror-vim'
+import {tags as t} from '@lezer/highlight'
+import Markdown from 'react-markdown'
+import type {Extension} from '@codemirror/state'
 
-export interface TextAreaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+const CodeMirror = lazy(() => import('@uiw/react-codemirror'))
 
-export const TextArea: React.FC<TextAreaProps> = ({className, ...props}) => {
+export const TextArea = ({className, ...props}) => {
+  const [toggleMarkdown, setToggleMarkdown] = useState(false)
+  const [content, setContent] = useState('')
+  const [theme, setTheme] = useState<Extension | null>(null)
+
+  const onChange = useCallback((val, viewUpdate) => {
+    console.log('val:', val)
+    setContent(val)
+  }, [])
+
+  useEffect(() => {
+    import('@uiw/codemirror-themes').then(mod => {
+      const createTheme = mod.createTheme
+      const newTheme: Extension = createTheme({
+        theme: 'dark',
+        settings: {
+          background: '#282828',
+          foreground: '#ebdbb2',
+          caret: '#ebdbb2',
+          selection: '#b99d555c',
+          lineHighlight: '#baa1602b',
+          gutterBackground: '#282828',
+          gutterForeground: '#7c6f64',
+        },
+        styles: [
+          {tag: t.keyword, color: '#fb4934'},
+          {
+            tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
+            color: '#8ec07c',
+          },
+          {tag: [t.variableName], color: '#83a598'},
+          {
+            tag: [t.function(t.variableName)],
+            color: '#b8bb26',
+            fontStyle: 'bold',
+          },
+          {tag: [t.labelName], color: '#ebdbb2'},
+          {
+            tag: [t.color, t.constant(t.name), t.standard(t.name)],
+            color: '#d3869b',
+          },
+          {tag: [t.definition(t.name), t.separator], color: '#ebdbb2'},
+          {tag: [t.brace], color: '#ebdbb2'},
+          {tag: [t.annotation], color: '#fb4934d'},
+          {
+            tag: [
+              t.number,
+              t.changed,
+              t.annotation,
+              t.modifier,
+              t.self,
+              t.namespace,
+            ],
+            color: '#d3869b',
+          },
+          {tag: [t.typeName, t.className], color: '#fabd2f'},
+          {tag: [t.operator, t.operatorKeyword], color: '#fb4934'},
+          {
+            tag: [t.tagName],
+            color: '#8ec07c',
+            fontStyle: 'bold',
+          },
+          {tag: [t.squareBracket], color: '#fe8019'},
+          {tag: [t.angleBracket], color: '#83a598'},
+          {tag: [t.attributeName], color: '#8ec07c'},
+          {tag: [t.regexp], color: '#8ec07c'},
+          {tag: [t.quote], color: '#928374'},
+          {tag: [t.string], color: '#ebdbb2'},
+          {
+            tag: t.link,
+            color: '#a89984',
+            textDecoration: 'underline',
+            textUnderlinePosition: 'under',
+          },
+          {tag: [t.url, t.escape, t.special(t.string)], color: '#d3869b'},
+          {tag: [t.meta], color: '#fabd2f'},
+          {tag: [t.comment], color: '#928374', fontStyle: 'italic'},
+          {tag: t.strong, fontWeight: 'bold', color: '#fe8019'},
+          {tag: t.emphasis, fontStyle: 'italic', color: '#b8bb26'},
+          {tag: t.strikethrough, textDecoration: 'line-through'},
+          {tag: t.heading, fontWeight: 'bold', color: '#b8bb26'},
+          {tag: [t.heading1, t.heading2], fontWeight: 'bold', color: '#b8bb26'},
+          {
+            tag: [t.heading3, t.heading4],
+            fontWeight: 'bold',
+            color: '#fabd2f',
+          },
+          {tag: [t.heading5, t.heading6], color: '#fabd2f'},
+          {tag: [t.atom, t.bool, t.special(t.variableName)], color: '#d3869b'},
+          {tag: [t.processingInstruction, t.inserted], color: '#83a598'},
+          {tag: [t.contentSeparator], color: '#fb4934'},
+          {
+            tag: t.invalid,
+            color: '#fe8019',
+            borderBottom: `1px dotted #fb4934d`,
+          },
+        ],
+      })
+
+      setTheme(newTheme)
+    })
+  }, [])
+
+  console.log(content, ' content')
+
   return (
-    <textarea
-      className={cx(
-        'w-full max-w-96 resize-none bg-white p-2 text-sm shadow-input',
-        className,
+    <>
+      <button
+        onClick={e => {
+          e.preventDefault()
+          setToggleMarkdown(!toggleMarkdown)
+        }}
+      >
+        Toggle
+      </button>
+      {toggleMarkdown ? (
+        <div className="prose max-w-none border p-2">
+          <Markdown>{content}</Markdown>
+        </div>
+      ) : (
+        <Suspense fallback={<div>Loading...</div>}>
+          <CodeMirror
+            value={content}
+            height="300px"
+            extensions={[
+              vim(),
+              markdown({base: markdownLanguage, codeLanguages: languages}),
+              ...(theme ? [theme] : []),
+            ]}
+            onChange={onChange}
+          />
+        </Suspense>
       )}
-      {...props}
-    />
+    </>
   )
 }

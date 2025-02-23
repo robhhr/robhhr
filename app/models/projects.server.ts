da8547f1814a1d765f990be3ff99321ec5713949
robhhr <robertohhr@pm.me>
2025-02-23 03:06:23 -0800

    add metadata support to create helper

--- app/models/projects.server.ts
+++ app/models/projects.server.ts
@@ -4,28 +4,33 @@ interface Project {
   status: string
   title: string
   content: string
+  meta: string
 }
 
 export async function createProject({
   status,
   title,
   content,
+  meta,
 }: Project): Promise<void> {
   const sql = `
     INSERT INTO projects (
       status, 
       title,
       content,
+      metadata,
       updated_at
     )
-    VALUES ($1, $2, $3, current_timestamp)
+    VALUES ($1, $2, $3, $4, current_timestamp)
     RETURNING id;
   `
 
   try {
-    const project = await query<Project>(sql, [status, title, content])
+    const project = await query<Project>(sql, [status, title, content, meta])
 
-    console.log(`inserted print record: ${JSON.stringify(project)}`)
+    console.log(
+      `inserted project titled ${title} and record: ${JSON.stringify(project)}`,
+    )
   } catch (error) {
     console.error('error inserting print:', error)
     throw error

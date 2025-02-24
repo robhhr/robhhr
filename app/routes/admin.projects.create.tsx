4e99480b6222425d86febd10543d2d448193ae3c
robhhr <robertohhr@pm.me>
2025-02-24 00:10:54 -0800

    basic error handling project form

--- app/routes/admin.projects.create.tsx
+++ app/routes/admin.projects.create.tsx
@@ -28,24 +28,27 @@ export const action = async ({request}: ActionFunctionArgs) => {
   const content = body.get('content') as string
   const meta = body.get('meta') as string
 
-  if (!status || !title || !content) {
-    return {error: 'all fields are required'}
-  }
+  // NOTE: meta excluded since its optional & status has default value
+  if (!title) return {error: 'title required'}
+  if (!content) return {error: 'content required'}
 
   try {
-    await createProject({status, title, content, meta})
+    const result = await createProject({status, title, content, meta})
+    return {
+      success: 'project created',
+      result,
+    }
   } catch (error) {
     console.error('error inserting project:', error)
     return {error: 'error creating project'}
   }
-
-  return {status, title, content, meta}
 }
 
 const DashboardProjectsCreate = () => {
   const [selected, setSelected] = useState('draft')
   const [error, setError] = useState<string | null>(null)
-  // const navigation = useNavigation()
+  const [success, setSuccess] = useState<string | null>(null)
+  const navigation = useNavigation()
   const actionData = useActionData<typeof action>()
   // console.log(actionData, ' actionData')
   // console.log(actionData && JSON.parse(actionData.meta))
@@ -56,6 +59,8 @@ const DashboardProjectsCreate = () => {
   useEffect(() => {
     if (actionData?.error) {
       setError(actionData.error)
+    } else if (actionData?.success) {
+      setSuccess(actionData.success)
     }
   }, [actionData])
 
@@ -96,18 +101,24 @@ const DashboardProjectsCreate = () => {
           <MarkdownEditor setError={setError} />
         </div>
 
-        <p>meta</p>
-
         <div className="mt-2 flex flex-col">
           <MetaControls />
         </div>
 
-        <Button className="mt-7" intent="admin" type="submit">
+        <Button
+          // disabled={navigation.state === 'submitting'}
+          disabled={navigation.state !== 'idle'}
+          className="mt-7"
+          intent="admin"
+          type="submit"
+        >
           create
         </Button>
       </Form>
 
-      <FeedbackDialog actionData={error ? {error} : undefined} />
+      <FeedbackDialog
+        actionData={error ? {error} : success ? {success} : undefined}
+      />
     </>
   )
 }

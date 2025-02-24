cf4fc97dd8c634cb81035aa211350500020ab7f5
robhhr <robertohhr@pm.me>
2025-02-24 00:09:28 -0800

    fix nav active state

--- app/components/admin/controls-projects.tsx
+++ app/components/admin/controls-projects.tsx
@@ -1,15 +1,28 @@
-import { Button } from '../modules/button'
+import {useLocation} from '@remix-run/react'
+import {Button} from '../modules/button'
 
 const ControlsProjects = () => {
+  const location = useLocation()
+
   return (
-      <ul className="flex">
-        <li className="mr-2">
-          <Button to="/admin/projects">view all</Button>
-        </li>
-        <li>
-          <Button to="/admin/projects/create">create project</Button>
-        </li>
-      </ul>
+    <ul className="flex">
+      <li className="mr-2">
+        <Button
+          disabled={location.pathname === '/admin/projects'}
+          to="/admin/projects"
+        >
+          view all
+        </Button>
+      </li>
+      <li>
+        <Button
+          disabled={location.pathname === '/admin/projects/create'}
+          to="/admin/projects/create"
+        >
+          create project
+        </Button>
+      </li>
+    </ul>
   )
 }
 

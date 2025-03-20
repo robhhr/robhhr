e3dd296126bab860a618e27a8843a6858c06b727
robhhr <robertohhr@pm.me>
2025-03-20 05:35:16 -0700

    migrate to tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+import tailwindcss from '@tailwindcss/vite'
 
 declare module "@remix-run/node" {
   interface Future {
@@ -18,5 +19,5 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
-  }), netlifyPlugin(), tsconfigPaths()],
+  }), netlifyPlugin(), tsconfigPaths(), tailwindcss()],
 });
